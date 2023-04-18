fae992572457907cabbae2193543913cb311b177
Mason <114454215+yearlongmason@users.noreply.github.com>
2023-04-18 15:28:12 -0400

    Add files via upload

---
+++ TitanicAnalysis.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[53]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+data = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+
+st.set_page_config(page_title='Titanic!')
+
+col1, col2, col3 = st.columns(3)
+
+fig, ax = plt.subplots(figsize=(7,5))
+ax.bar(['1st', '2nd', '3rd'], [len(data[data['Pclass']==1]), len(data[data['Pclass']==2]), len(data[data['Pclass']==3])],       linewidth=1, edgecolor='Black')
+ax.set_title('Passenger classes')
+data
+
+with col1:
+    st.pyplot(fig)
+
+
+# In[54]:
+
+
+fig, ax = plt.subplots(figsize=(7,5))
+ax.hist(data['Age'], color='tab:purple', linewidth=1, edgecolor='Black')
+ax.set_title('Passenger Age')
+
+with col2:
+    st.pyplot(fig)
+
+
+# In[55]:
+
+
+fig, ax = plt.subplots(figsize=(7,5))
+sns.violinplot(data=data, x='Pclass', y='Age', ax=ax)
+ax.set_title('Passenger Age by class')
+
+with col3:
+    st.pyplot(fig)
+
